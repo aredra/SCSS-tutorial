08053b22f31855c6e7df01bb0c4e77d6ab2cca0c
jy.Seo <dev1.wizdome.co.kr>
2019-11-26 22:03:29 +0900

    start scss

---
+++ src/component/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Button.scss';
+
+function Button({children}) {
+    return (
+        <button className="Button">
+            {children}
+        </button>
+    )
+}
+
+export default Button;
