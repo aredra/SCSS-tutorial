e081f86005f8c2d05f094adf9ee713e7d8a1bc5b
aredra <rnswn48@gmail.com>
2019-11-27 09:57:56 +0900

    add buttons mixin classname.lib

--- src/App.js
+++ src/App.js
@@ -5,8 +5,20 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <div className="Button">
+      <div className="Buttons">
+        <Button size="large" color="gray">Button</Button>
+        <Button color="gray">Button</Button>
+        <Button size="small" color="gray">Button</Button>
+      </div>
+      <div className="Buttons">
+        <Button size="large">Button</Button>
         <Button>Button</Button>
+        <Button size="small">Button</Button>
+      </div>
+      <div className="Buttons">
+        <Button size="large" color="pink">Button</Button>
+        <Button color="pink">Button</Button>
+        <Button size="small" color="pink">Button</Button>
       </div>
     </div>
   );
--- src/component/Button.js
+++ src/component/Button.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import classNames from 'classnames';
 import './Button.scss';
 
-function Button({children}) {
+// size: large, medium, small
+// `Button ${size}`
+function Button({children, size, color}) {
     return (
-        <button className="Button">
+        <button className={classNames("Button", size, color)}>
             {children}
         </button>
     )
+}
+
+Button.defaultProps = {
+    size: "medium",
+    color: "blue"
 }
 
 export default Button;
